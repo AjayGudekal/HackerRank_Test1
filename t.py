3d5456962f96b051ee3e40c92e0b21ee888cae4a
AjayGudekal <89381861+AjayGudekal@users.noreply.github.com>
2021-09-09 20:52:54 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,30 @@
+def closestNumbers(numbers):
+    d = []
+    pos = 0
+    for i in numbers:
+        pos += 1
+
+        for j in range(pos, len(numbers)):
+            v = []
+            k = i - numbers[j]
+            v.append(abs(k))
+            v.append(i)
+            v.append(numbers[j])
+            d.append(v)
+    d.sort()
+    c = d[0][0]
+    # print(d)
+    for i in d:
+        res=[]
+        if i[0] == c:
+            res.append(i[1])
+            res.append(i[2])
+            res.sort()
+            print(str(res[0]) + ' ' + str(res[1]))
+if __name__ == '__main__':
+    numbers_count = int(input())
+    numbers = []
+    for _ in range(numbers_count):
+        numbers_item = int(input())
+        numbers.append(numbers_item)
+    closestNumbers(numbers)
